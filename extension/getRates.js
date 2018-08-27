1956e67bdf9b5ccce348e8fd99a7d0f7d25bb121
uridanan <uridanan@gmail.com>
2018-08-27 21:39:21 +0300

    Finalize getRate
    and add examples from existing extensions

--- extension/getRates.js
+++ extension/getRates.js
@@ -1,49 +1,57 @@
-//getItemPage.js
+//getRates.js
 
 //https://free.currencyconverterapi.com/api/v5/convert?q=EUR_USD&compact=y
 //Get exchange rate from api
 function getRate(from,to){
-  var currencyPair = from+"_"+to;
-  var url = "https://free.currencyconverterapi.com/api/v5/convert?q="+currencyPair+"&compact=y";
+  var url = getRateRequestURL(from,to);
   var x = new XMLHttpRequest();
   x.overrideMimeType('text/json');
   console.log("getRate from: " + url);
   x.open('GET', url);
   x.onreadystatechange = function() {
     if (x.readyState == 4 && x.status == 200) {
-      processResponse(x.responseText,currencyPair);
+      processResponse(x.responseText,from,to);
       //alert(x.responseText);
     }
   };
   x.send();
 }
 
-function processResponse(response,currencyPair){
+function processResponse(response,from,to){
   //add code to handle response here
   //parse response HTML and find the first search result
   console.log("process xhr response");
   console.log(response);
-  rate = extractRate(response,currencyPair);
+  //{EUR_USD: {val: 1.161595}}
+  rate = extractRate(response,from,to);
   console.log(rate);
 
-  //text = extractTextCell(response);
-  //console.log(text);
-
-  //xmlDoc = jQuery.parseXML(text);
-  //target = getFirstLink(xmlDoc);
+  document.getElementById("demo").innerHTML = "Rate from EUR to USD: " + rate;
   //sendMessage("searchresult", target, cat);
+}
+
+function extractRate(response,from,to){
+  var currencyPair = getCurrencyPair(from,to);
+  var myObj = JSON.parse(response);
+  var rate = myObj[currencyPair].val;
+  return rate;
+}
+
+function getCurrencyPair(from,to){
+  return from+"_"+to;
+}
+
+function getRateRequestURL(from,to){
+  var currencyPair = getCurrencyPair(from,to);
+  var url = "https://free.currencyconverterapi.com/api/v5/convert?q="+currencyPair+"&compact=y";
+  return url;
 }
 
 getRate("EUR","USD")
 
-function extractRate(response,currencyPair){
-  rate = "";
-  var myObj = JSON.parse(response);
-  rate = myObj[currencyPair].val;
-  return rate;
-}
 
-//{EUR_USD: {val: 1.161595}}
+
+
 
 
 function sendMessage(msg, url, cat){
